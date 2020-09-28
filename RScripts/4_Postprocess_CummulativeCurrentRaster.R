10c9c89701356d672743d10d12ca90e5baca8854
bronwynrayfield <bronwyn.rayfield@apexrms.com>
2020-09-28 14:19:27 -0400

    Update 4_Postprocess_CummulativeCurrentRaster.R
    Updated circuit analyses

--- RScripts/4_Postprocess_CummulativeCurrentRaster.R
+++ RScripts/4_Postprocess_CummulativeCurrentRaster.R
@@ -8,25 +8,45 @@ projectDir <- "C:/Users/bronw/Documents/Apex/Projects/Active/A233_RBGConnectivit
 dataDir <- file.path(projectDir, "Data/Processed")
 resultsDir <- file.path(projectDir, "Results")
 
-# Read in data
-studyArea <- st_read(file.path(dataDir, paste0("studyarea_", polygonBufferWidth, "km_unbuffered.shp")))
-currMap <- raster(file.path(resultsDir,"try_cum_curmap.tif"))
+analysisName <- "Generic_CulvertBridge_5pairwise"
 
-crs(currMap) <- crs(studyArea)
-logcurrMap <- log(currMap)
+# Read in data
+studyArea <- st_read(file.path(dataDir, paste0("StudyArea_20km.shp")))
+focalArea <- st_read(file.path(dataDir, "FocalArea.shp"))
+currMap_buffer <- raster(file.path(resultsDir,paste0(analysisName, "_cum_curmap.asc")))
+
+crs(currMap_buffer) <- crs(studyArea)
+logcurrMap_buffer <- log(currMap_buffer)
 
 # Crop to study area
-currMap_unbuffered <- currMap %>%
+currMap <- currMap_buffer %>%
   crop(., extent(studyArea), snap="out") %>% # Crop to study area extent
   mask(., mask=studyArea) %>% # Clip to study area
   trim(.)
 
-logcurrMap_unbuffered <- logcurrMap %>%
+logcurrMap <- logcurrMap_buffer %>%
   crop(., extent(studyArea), snap="out") %>% # Crop to study area extent
   mask(., mask=studyArea) %>% # Clip to study area
   trim(.)
 
-writeRaster(currMap, file.path(resultsDir,"try_cum_curmap_bufferred.tif"), overwrite=TRUE)
-writeRaster(logcurrMap, file.path(resultsDir,"try_cum_curmap_bufferred_log.tif"), overwrite=TRUE)
-writeRaster(currMap_unbuffered, file.path(resultsDir,"try_cum_curmap_unbufferred.tif"), overwrite=TRUE)
-writeRaster(logcurrMap_unbuffered, file.path(resultsDir,"try_cum_curmap_unbufferred_log.tif"), overwrite=TRUE)
+# Crop to focal area
+currMap_focalArea <- currMap_buffer %>%
+  crop(., extent(focalArea), snap="out") %>% # Crop to focal area extent
+  mask(., mask=focalArea) %>% # Clip to focal area
+  trim(.)
+
+logcurrMap_focalArea <- logcurrMap_buffer %>%
+  crop(., extent(focalArea), snap="out") %>% # Crop to focal area extent
+  mask(., mask=focalArea) %>% # Clip to focal area
+  trim(.)
+
+# Save geotifs
+# Study Area buffer
+writeRaster(currMap_buffer, file.path(resultsDir, paste0(analysisName, "_cum_curmap_20km_buffer.tif")), overwrite=TRUE)
+writeRaster(logcurrMap_buffer, file.path(resultsDir, paste0(analysisName, "_cum_curmap_log_20km_buffer.tif")), overwrite=TRUE)
+# Study Area
+writeRaster(currMap, file.path(resultsDir, paste0(analysisName, "_cum_curmap_20km.tif")), overwrite=TRUE)
+writeRaster(logcurrMap, file.path(resultsDir, paste0(analysisName, "_cum_curmap_log_20km.tif")), overwrite=TRUE)
+# Focal Area
+writeRaster(currMap_focalArea, file.path(resultsDir, paste0(analysisName, "_cum_curmap_focalArea.tif")), overwrite=TRUE)
+writeRaster(logcurrMap_focalArea, file.path(resultsDir, paste0(analysisName, "_cum_curmap_log_focalArea.tif")), overwrite=TRUE)
