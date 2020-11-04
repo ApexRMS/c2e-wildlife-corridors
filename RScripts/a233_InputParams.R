c5b3a7a9a3f8f7833e660795b1887c089bcf87c9
cmtucker <carolinemtucker@gmail.com>
2020-11-04 12:48:29 -0800

    upload input params

---
+++ RScripts/a233_InputParams.R
@@ -0,0 +1,28 @@
+######################################################################
+# a233 Royal Botanical Gardens Wildlife Corridor Mapping            
+#  Headers, input parameter values   
+#  09/2020                                                                          
+#                                                                  
+# Script created by C Tucker  for ApexRMS 
+#####################################################################
+
+
+## Input parameters---------------------------------------------------------
+
+  # Species
+specieslist <- c("BLBR", "ODVI", "EMBL")
+
+  # Final values used
+  
+polygonBufferWidth <- 20 # In km
+# in km : the study area is made by buffering the polygon that encompases the Cootes to Escarpment EcoPark System
+
+roadbuffer <- 12 #in m: Buffer road lines by constant so that rasterizing aligns with roads in SOLRIS
+
+minCulvertSize <- 0.05
+# About half of the OR values fall less than 0.015. According to Halton Road Ecology paper, values  must be > 0.05 for even smallest mammals    
+
+culvertResistancevalue <- 10 # all suitable culverts given value of 10
+bridgeResistancevalue <- 10 # all suitable bridges given value of 10
+
+suitabilityThreshold <- 60
